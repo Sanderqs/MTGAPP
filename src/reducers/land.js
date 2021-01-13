63fac45f46bdcc02aab836d33bcf8243425fcb10
Stephan van Eijkelenburg <stephanvane@gmail.com>
2021-01-14 00:05:43 +0100

    .

--- src/reducers/land.js
+++ src/reducers/land.js
@@ -1,21 +1,25 @@
 const initialState = {
   land: 17,
   nonland: 23,
+  // total: 40,
 };
 
 export default function landReducer(state = initialState, action) {
   switch (action.type) {
     case "DRAFT":
-      return { ...state, land: 17, nonland: 23 };
+      return { ...state, land: 17, nonland: 23, total: 40 };
 
     case "STANDARD":
-      return { ...state, land: 23, nonland: 37 };
+      return { ...state, land: 23, nonland: 37, total: 60 };
     case "EDH":
-      return { ...state, land: 44, nonland: 55 };
+      return { ...state, land: 44, nonland: 55, total: 99 };
     case "INPUT_CHANGE":
-      const { card, type } = action.payload;
-      return { ...state, [card]: type };
-
+      const { type, numberOfCards } = action.payload;
+      return { ...state, [type]: numberOfCards };
+    case "TOTAL_CHANGE": {
+      const total = action.payload;
+      return { ...state, land: total - state.nonland };
+    }
     default:
       return state;
   }
@@ -29,6 +33,10 @@ export const setStandard = (land, nonland) => {
 export const setEdh = (land, nonland) => {
   return { type: "EDH", payload: { land, nonland } };
 };
-export const updateCard = (card, type) => {
-  return { type: "INPUT_CHANGE", payload: { card, type } };
+export const updateCard = (type, numberOfCards) => {
+  return { type: "INPUT_CHANGE", payload: { type, numberOfCards } };
+};
+
+export const updateTotal = (numberOfCards) => {
+  return { type: "TOTAL_CHANGE", payload: numberOfCards };
 };
