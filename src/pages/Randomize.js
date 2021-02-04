6cfde544f6bbbe3873b772c48cf9ee7883ad2184
Sanderqs <channie.qs@gail.com>
2021-02-04 23:40:29 +0100

    d6 d20 toegevoegd
    moet nog art bij

--- src/pages/Randomize.js
+++ src/pages/Randomize.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Randomize = () => {
-  return <div>hallo randomize</div>;
+  function rollDice(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+  }
+  // for (let i = 0; i < 5; i++);
+
+  const rollDice6 = () => rollDice(1, 6);
+  const rollDice20 = () => rollDice(1, 20);
+  return (
+    <div>
+      hallo randomize {rollDice20()}
+      <br></br>
+      {rollDice6()} ;
+    </div>
+  );
 };
 
 export default Randomize;
