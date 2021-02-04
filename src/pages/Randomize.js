c45e9dcd4500ee59fc51f6522ac02b8c8759d853
Sanderqs <channie.qs@gail.com>
2021-02-05 00:54:21 +0100

    D6 met plaatjes

--- src/pages/Randomize.js
+++ src/pages/Randomize.js
@@ -1,18 +1,26 @@
 import React from "react";
+// import * from '../assets'
 
 const Randomize = () => {
-  function rollDice(min, max) {
+  const rollDice = (min, max) => {
     return min + Math.floor(Math.random() * (max - min + 1));
-  }
+  };
   // for (let i = 0; i < 5; i++);
 
   const rollDice6 = () => rollDice(1, 6);
   const rollDice20 = () => rollDice(1, 20);
+
+  const diceImage = require("../assets/dice" + rollDice6() + ".png");
   return (
     <div>
-      hallo randomize {rollDice20()}
+      {/* hallo randomize {rollDice20()}
       <br></br>
-      {rollDice6()} ;
+      {diceImage} */}
+      <img
+        className="Dice"
+        src={require(`../assets/${"dice" + rollDice6()}.png`).default}
+        alt="kees"
+      />
     </div>
   );
 };
